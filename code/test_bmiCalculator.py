09c68b7dda02c83cfa16d37fef56d084bfef90a9
Jacob <jra457@msstate.edu>
2024-03-07 19:31:10 -0600

    Added unit tests for BMI category function

--- code/test_bmiCalculator.py
+++ code/test_bmiCalculator.py
@@ -6,30 +6,41 @@ Run: pytest test_bmiCalculator.py
 """
 import unittest
 import numpy as np
-from bmiCalculator import weight, height, bmi
+from bmiCalculator import weight, height, bmi, bmiCategory
 
 # Average male height: 5'9" = 69"
-# Average female height: 5'4" = 64"
 
 class TestCalculator(unittest.TestCase):
 
   def test_weight(self):
-    # Test at weight = 1
-    self.assertEqual(weight(1), 0.45)
+    # ~ Underweight Boundary Test
+    # Test at weight = 122.1
+    self.assertEqual(weight(122.1), np.multiply(122.1, 0.45))
+
+    # ~ Healthy Weight Boundary Test
+    # Test at weight = 122.4
+    self.assertEqual(weight(122.4), np.multiply(122.4, 0.45))
+    # Test at weight = 164.6
+    self.assertEqual(weight(164.6), np.multiply(164.6, 0.45))
+
+    # ~ Overweight Boundary Test
+    # Test at weight = 164.7
+    self.assertEqual(weight(164.7), np.multiply(164.7, 0.45))
+    # Test at weight = 197.7
+    self.assertEqual(weight(197.7), np.multiply(197.7, 0.45))
+
+    # ~ Obese Boundary Test
+    # Test at weight = 197.8
+    self.assertEqual(weight(197.8), np.multiply(197.8, 0.45))
+
 
 
   def test_height(self):
-    # Test at height = 1
-    self.assertEqual(height(1), np.power(0.025, 2))
+    self.assertEqual(height(69), np.power(np.multiply(69, 0.025), 2))
+
 
 
   def test_bmi(self):
-    # Test at weight & height = 1
-    self.assertEqual(bmi(1, 1), np.divide(0.45, np.power(0.025, 2)))
-  
-    # Test at weight = 1, height = 69"
-    self.assertEqual(bmi(1, 69), np.divide(np.multiply(1, 0.45), np.power(np.multiply(69, 0.025), 2)))
-
     # ~ Underweight Boundary Test
     # Test at weight = 122.1, height = 69"
     self.assertEqual(bmi(122.1, 69), np.divide(np.multiply(122.1, 0.45), np.power(np.multiply(69, 0.025), 2)))
@@ -49,6 +60,29 @@ class TestCalculator(unittest.TestCase):
     # ~ Obese Boundary Test
     # Test at weight = 197.8, height = 69"
     self.assertEqual(bmi(197.8, 69), np.divide(np.multiply(197.8, 0.45), np.power(np.multiply(69, 0.025), 2)))
+
+
+
+  def test_bmiCategory(self):
+    # ~ Underweight Boundary Test
+    # BMI = 18.4
+    self.assertEqual(bmiCategory(18.4), "Underweight")
+
+    # ~ Healthy Weight Boundary Test
+    # BMI = 18.5
+    self.assertEqual(bmiCategory(18.5), "Healthy weight")
+    # BMI = 24.8
+    self.assertEqual(bmiCategory(24.8), "Healthy weight")
+
+    # ~ Overweight Boundary Test
+    # BMI = 25.0
+    self.assertEqual(bmiCategory(25.0), "Overweight")
+    # BMI = 24.8
+    self.assertEqual(bmiCategory(29.8), "Overweight")
+
+    # ~ Obese Boundary Test
+    # BMI = 29.9
+    self.assertEqual(bmiCategory(29.9), "Obese")
 
 
 
